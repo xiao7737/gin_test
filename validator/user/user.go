5a422ad13346f7b84f9b1cb64c368da43c817e3b
xiao7737 <773708486@qq.com>
2020-04-05 17:16:27 +0800

    验证器利用的的反射原理1

--- validator/user/user.go
+++ validator/user/user.go
@@ -9,7 +9,7 @@ import (
 func NameValid(v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
 	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
 ) bool {
-	// 利用反射，将反射类型对象转换为借口类型变量
+	// 利用反射，将反射类型对象转换为接口类型变量
 	// 通过断言，恢复底层的具体值
 	if s, ok := field.Interface().(string); ok {
 		if s == "admin" || len(s) <= 6 {
