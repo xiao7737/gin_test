3f8b6103eb49f6860948976c236d179c15650fb2
xiao7737 <773708486@qq.com>
2020-04-06 16:25:11 +0800

    利用go test做性能分析和内存分析

--- main.go
+++ main.go
@@ -23,3 +23,14 @@ func main() {
 //火炬图
 // 在项目目录生成touch.svg，内存临时分配情况
 //	go-torch -alloc_space http://127.0.0.1:8080/debug/pprof/heap --colors=mem
+
+// 利用go test作性能分析
+//	 go test -bench . -benchmem -cpuprofile cpu.prof 获取CPU性能数据 或者 go test -bench=. -cpuprofile=cpu.prof
+//        go tool pprof cpu.prof
+//	 go test -bench . -benchmem -memprofile mem.prof    获取内存性能数据
+// 		  go tool pprof men.prof
+
+//  pprof 命令下
+// 输入:list 函数名      查看函数具体耗时
+// 输入：web  查看展示图
+// 输入：svg  生成svg图像
