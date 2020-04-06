c094b44de7c9144aef38ebde1d62c3b5c9ea21ab
xiao7737 <773708486@qq.com>
2020-04-06 18:42:59 +0800

    gin路由树的节点形成请求处理链

--- api/router/router.go
+++ api/router/router.go
@@ -27,12 +27,13 @@ func InitRouter() *gin.Engine { // Engine结构体包含了 RouterGroup
 }
 
 // RouterGroup是对路由树的包装
+// 路由树的每个节点会挂载若干函数和中间件构成一个函数处理链
 // Engine 结构体继承了 RouterGroup ，所以 Engine 直接具备了 RouterGroup 所有的路由管理功能
 // 这是为什么在 Hello World例子中，可以直接使用 Engine 对象来定义路由规则。同时 RouteGroup 对象里面还会包含一个 Engine 的指针
 // 通过调用 Engine.addRoute 方法将请求处理器挂接到路由树中，路由规则被分成9颗前缀树，对应9中http方法
 // context可以看做gin对http.Request对象的包装
 // Gin不支持https，官方建议用Nginx转发https请求到Gin
 // 路由节点挂载函数链：Gin提供了中间件，只有函数链的尾部才是业务处理
-// 请求流程：接到请求时，在路由树找到相应的处理链，构造一个Context对象，依次调用Next()方法进行请求处理
+// 请求流程：接到请求时，在路由树找到相应的节点，组成请求处理链，构造一个Context对象，依次调用Next()方法进行请求处理
 // Abort()中断请求链，只会中断当前操作，后续的中间件或操作会继续执行，原理：将context.index设置较大,让Next()的调用循环立即结束
 // Use() 注册中间件
