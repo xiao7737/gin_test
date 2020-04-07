4c875cbad188aafbaf5dce135378a9598a5f5960
xiao7737 <773708486@qq.com>
2020-04-07 19:23:48 +0800

    自定义返回状态码

--- msg/msg.go
+++ msg/msg.go
@@ -4,6 +4,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	SUCCESS         = 200
+	INVALID_PARAMS  = 400
+	ERROR           = 500
+	NO_ROWS         = 4001
+	VALID_FAILED    = 4002
+	INSERT_FALIED   = 4003
+	DELETE_FALIED   = 4004
+	UPDATE_FALIED   = 4005
+	ERROR_EXIST_TAG = 10001
+)
+
 var msgMap = map[int]string{
 	SUCCESS:         "ok",
 	ERROR:           "fail",
@@ -23,7 +35,7 @@ type Gin struct {
 func (g *Gin) Response(httpCode int, errCode int, data interface{}) {
 	g.C.JSON(httpCode, gin.H{
 		"code": httpCode,
-		"msg":  GetErrorMsg(errCode), //也可以采用http包的statusText(code)
+		"msg":  GetErrorMsg(errCode), //也可以采用http包的statusText(code)，自定义的更香~~
 		"data": data,
 	})
 	return
